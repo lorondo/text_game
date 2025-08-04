daa725657a8694bd78806fca81c0dcdfbf4d30b0
lorondo <mralexanderlindsay@gmail.com>
2025-08-04 15:55:47 -0700

    house rpg test

--- practice_rpgs/house_rpg.py
+++ practice_rpgs/house_rpg.py
@@ -2,3 +2,19 @@ inventory = []
 
 currentRoom = "Hall"
 
+rooms = {
+  "Hall": {
+    "south": "Kitchen" ## when you're in the Hall, the value of south is Kitchen
+  },
+  "Kitchen": {
+    "north": "Hall" ## when you're in the Kitchen, the value of north is Hall
+  } 
+}
+
+print("I am in the", currentRoom)
+
+directions = rooms[currentRoom]
+
+currentRoom = directions
+
+print("I am NOW in the", currentRoom)
