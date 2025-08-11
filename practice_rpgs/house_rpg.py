15bdf26aa3bd3deb72b2ef800c538b0b5540fd4e
lorondo <mralexanderlindsay@gmail.com>
2025-08-10 18:06:08 -0700

    added get and go commands for moving between rooms and interacting with items in the rooms

--- practice_rpgs/house_rpg.py
+++ practice_rpgs/house_rpg.py
@@ -1,6 +1,6 @@
 inventory = []
 
-currentRoom = "Hall"
+currentRoom = "Kitchen"
 
 rooms = {
   "Hall": {
@@ -13,10 +13,28 @@ rooms = {
 }
 
 while True:
+  print(f"You are in the {currentRoom}!")
+
   move = input(">") # "get sword", "go north"
   move = move.split(" ", 1) # "get sword" -> ["get", "sword"]
+  
   # move[0] -> "get"
   if move[0] == "get":
     if move[1] == rooms[currentRoom]["item"]:
+      print(f"You got a {move[1]}!")
+      inventory.append(move[1])
+      rooms[currentRoom]["item"] = ""
+      print(inventory)
+    else:
+      print(f"You don't see a {move[1]} here!")
+
+  # move[0] -> "go"
+  if move[0] == "go":
+    if move[1] in rooms[currentRoom]:
+      currentRoom = rooms[currentRoom][move[1]]
+      print(f"You are now in the {currentRoom}!")
+    else: 
+      print(f"You can't go {move[1]}!")
+
   
 
